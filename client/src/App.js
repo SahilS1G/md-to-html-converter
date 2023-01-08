fa185b9970e29cf3ccf618cf7c5db42721cb1ff0
s0hamn <soham.naigaonkar@gmail.com>
2023-01-08 22:46:56 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 import Blogs from './components/Blogs';
 
 function App() {
+
+  // fetch(dir)
+  //   .then(row => row.text())
+  //   .then(text => {
+  //     console.log('text:', text);
+  //   });
+
   return (
+
     <div className="App">
       <Blogs />
 
